095ae374e5ebb60fdeae3b8405d895b9d8b0272d
Paula <alejandrapmp17@gmail.com>
2024-08-28 17:44:34 -0500

    feat: :sparkles: Props module was fixed

--- AnimeDP/src/Controller/PropsController.java
+++ AnimeDP/src/Controller/PropsController.java
@@ -4,11 +4,14 @@ package Controller;
 import Dao.PropsDao;
 import Model.PropsModel;
 import java.util.List;
+import java.util.Scanner;
 
 
 public class PropsController {
     
     private PropsDao propsDao;
+    private Scanner scanner;
+
 
     public PropsController() {
         this.propsDao = new PropsDao();
@@ -24,5 +27,64 @@ public class PropsController {
   
    public List<PropsModel> getAllProps(){
        return propsDao.getAllProps();
+   }
+   
+   public PropsModel getPropsById(int propsId){
+       return propsDao.getPropsById(propsId);
+   }
+   
+   public void updatePropsStatus(){
+        System.out.print("Ingrese el ID de la utilería a actualizar: ");
+        int propsId = scanner.nextInt();
+        scanner.nextLine(); 
+        
+        PropsModel props = propsDao.getPropsById(propsId);
+        if (props == null) {
+            System.out.println("Evento con ID " + propsId + " no encontrado.");
+            return;
+        }
+        
+        System.out.println("\n-----------------------------------------");
+        System.out.println(" Estado actual de la utilería: " + props.getStatus());
+        System.out.println("-----------------------------------------");
+        
+        System.out.println("\n-----------------------------------------");
+        System.out.println("  Ingrese el nuevo estado de la utilería:  ");
+        System.out.println("                                           ");
+        System.out.println("    1. En el sitio                         ");
+        System.out.println("    2. En el almacen                       ");
+        System.out.print("      Seleccione una opción:                 ");
+        System.out.println("-------------------------------------------");
+        
+        int optionS;
+        String newStatus = "";
+        
+        while (true) {
+            try {
+                optionS = scanner.nextInt();
+                scanner.nextLine();
+                if (optionS == 1){
+                    newStatus = "En sitio"; 
+                    break;
+                }else if(optionS == 2){
+                    newStatus = "En almacen";
+                    break;
+                }else{
+                    System.out.println("Opción no valida. Por favor, seleccione 1 o 2.");
+                }
+            }catch (Exception e) {
+                System.out.println("Entrada no valida. Por favor, ingrese un número entero.");
+                scanner.next();
+            }
+        }
+        
+        props.setStatus(newStatus);
+        if(propsDao.updatePropsStatus(props)){
+            System.out.println("Estado de la utilería actualizado exitosamente a " + newStatus + ".");
+        }else {
+            System.out.println("Error al actualizar el estado de la utilería . Por favor, intente de nuevo.");
+        }
+        
+        
    }
 }
--- AnimeDP/src/View/PropsView.java
+++ AnimeDP/src/View/PropsView.java
@@ -19,11 +19,17 @@ public class PropsView {
         
         int option;
         do {
-            System.out.println("\nGestión de Utilería:");
-            System.out.println("1. Añadir Utilería");
-            System.out.println("2. Mostrar todas las Utilerías");
-            System.out.println("3. Actualizar Estado de Utilería");
-            System.out.println("4. Salir");
+            System.out.println("-----------------------------------------");
+            System.out.println("'         Gestión de Utilería:          '");
+            System.out.println("'                                       '");
+            System.out.println("'    1. Añadir Utilería                 '");
+            System.out.println("'    2. Mostrar todas las Utilerías     '");
+            System.out.println("'    3. Mostrar todas las Utilerías     '");
+            System.out.println("'    4. Actualizar Estado de Utilería   '");
+            System.out.println("'    5. Actualizar Cantidad de Utilería '");
+            System.out.println("'    6. Eliminar Utilería               '");
+            System.out.println("'    7. Salir                           '");
+            System.out.println("-----------------------------------------");
             System.out.print("Seleccione una opción: ");
             option = scanner.nextInt();
             scanner.nextLine(); // Consume la nueva línea
@@ -31,22 +37,27 @@ public class PropsView {
             switch (option) {
                 case 1 -> addProps();
                 case 2 -> showAllProps();
-                case 3 -> System.out.println("Actualizar");
-                case 4 -> System.out.println("Saliendo...");
+                case 3 -> showPropsById();
+                case 4 -> updatePropsStatus();
+                case 5 -> System.out.println("Actualizar");
+                case 6 -> System.out.println("eliminar");
+                case 7 -> System.out.println("Saliendo...");
                 default -> System.out.println("Opción no válida, intente de nuevo.");
             }
-        } while (option != 4);
+        } while (option != 7);
     }
     
     private void addProps() {
-        System.out.println("\nAñadir nueva utilería:");
+        System.out.println("\n-----------------------------------------");
+        System.out.println("           Añadir nueva utilería           ");
+        System.out.println("-------------------------------------------");
         
         int eventId;
         
         // Validar el ID del evento
         while (true) {
             try {
-                System.out.print("Id del evento al que pertenece la utileria a crear: ");
+                System.out.println("Id del evento al que pertenece la utilería a crear: ");
                 eventId = scanner.nextInt();
                 scanner.nextLine();
                 
@@ -68,15 +79,17 @@ public class PropsView {
         int quantity = scanner.nextInt();
         scanner.nextLine();
         
-        System.out.println("Ingrese el estado de la utilería: ");
-        System.out.println("1. En el sitio ");
-        System.out.println("2. En el almacen");
-        System.out.print("Seleccione una opción: ");
+        System.out.println("\n-----------------------------------------");
+        System.out.println("     Ingrese el estado de la utilería:     ");
+        System.out.println("                                           ");
+        System.out.println("    1. En el sitio                         ");
+        System.out.println("    2. En el almacen                       ");
+        System.out.print("      Seleccione una opción:                 ");
+        System.out.println("-------------------------------------------");
         
         int optionS;
         String status = "";
         
-        OUTER:
         while (true) {
             try {
                 optionS = scanner.nextInt();
@@ -108,15 +121,57 @@ public class PropsView {
     }
     
     private void showAllProps() {
-        System.out.println("\nListado de toda la Utilería:");
+        System.out.println("\n----------------------------------------");
+        System.out.println("     Listado de toda la Utilería:          ");
+        System.out.println("------------------------------------------");
         propsController.getAllProps().forEach(prop -> System.out.println(
                 
-                "ID: " + prop.getId() +
+                "-> ID: " + prop.getId() +
                 ", Nombre: " + prop.getName() +
                 ", Cantidad: " + prop.getQuantity() +
                 ", Estado: " + prop.getStatus() +
-                ", EnventoID: " + prop.getEventId()));
-                        
+                ", EnventoID: " + prop.getEventId()+ "\n"));                
+    }
+    
+    private void showPropsById(){
+        System.out.println("\n----------------------------------------");
+
+        int propsId;
+
+        // Validar el ID de la utilería
+        while (true) {
+            try {
+                System.out.println("Id de la utilería a mostrar: ");
+                propsId = scanner.nextInt();
+                scanner.nextLine();
+                
+                PropsModel props = propsController.getPropsById(propsId);
+                
+                if(props != null){
+                    System.out.println("Detalles de la utilería:");
+                    System.out.println("ID: " + props.getId());
+                    System.out.println("Nombre: " + props.getName());
+                    System.out.println("Cantidad: " + props.getQuantity());
+                    System.out.println("Estado: " + props.getStatus());
+                    System.out.println("ID del Evento: " + props.getEventId());
+                    break; // Si no hay excepción, salir del bucle
+                }else{
+                    System.out.println("El evento no se encuentra registrado");
+                } 
+            } catch (IllegalArgumentException e) {
+                System.out.println("Error: " + e.getMessage());
+                System.out.println("Por favor, introduzca un ID de utilería diferente.");
+            }
+        }
+        System.out.println("------------------------------------------");
+    }
+      
+    private void updatePropsStatus() {
+        try {
+            propsController.updatePropsStatus();
+        } catch (Exception e) {
+            System.out.println("Se produjo un error al intentar actualizar el estado de la utilería: " + e.getMessage());
+        }
     }
     
     public static void main(String[] args) {
